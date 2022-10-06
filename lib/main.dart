9430c779635f6c3c88ffb66e86fffe9dc721b727
vivekmishraa12 <v-vivek.mishra@nowfloats.com>
2022-10-06 19:41:22 +0530

    assests

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sample/ravi/MyAppStart.dart';
+import 'package:flutter_sample/vivek_official/news_project/main/my_app.dart';
 
 void main() {
-  runApp(const MyAppStart());
+  runApp(const MyApp2());
 }
