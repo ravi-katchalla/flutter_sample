2254704f71a0b159eb588209163e8650e5a610d3
vivekmishraa12 <v-vivek.mishra@nowfloats.com>
2022-10-14 14:32:38 +0530

    sockect merge

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,8 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_plus_macos
-<<<<<<< HEAD
 import emoji_picker_flutter
-=======
 import location
->>>>>>> cbd5cf15a07ee74aab1b9bab9f691f13bcf22506
 import path_provider_macos
 import shared_preferences_macos
 import sqflite
@@ -18,11 +15,8 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
-<<<<<<< HEAD
   EmojiPickerFlutterPlugin.register(with: registry.registrar(forPlugin: "EmojiPickerFlutterPlugin"))
-=======
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
->>>>>>> cbd5cf15a07ee74aab1b9bab9f691f13bcf22506
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
